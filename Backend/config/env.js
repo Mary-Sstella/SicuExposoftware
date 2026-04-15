8aeae11b5b86572cea72742b3cd0b4ad962f3447
SstefanyPerez <TU_CORREO_DE_GITHUB>
2026-04-15 10:38:16 -0500

    Configuracion Database

---
+++ Backend/config/env.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+module.exports = {
+    db: {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        name: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+    }
+}
