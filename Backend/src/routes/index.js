ad2f2dbbc303590170c144a6d0c22cce47838e44
SstefanyPerez <TU_CORREO_DE_GITHUB>
2026-04-16 17:24:03 -0500

    Autenticaion con JWT desde el Backend

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const userRoutes = require('../modules/users/user.routes')
 const estudianteRoutes = require('../modules/estudiantes/estudiante.routes')
+const authRoutes = require('../modules/auth/auth.routes')
 
 const router = Router()
 
+router.use('/auth', authRoutes)
 router.use('/users', userRoutes)
 router.use('/estudiantes', estudianteRoutes)
 
