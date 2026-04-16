0a72bddfeb38efd0d2be0b03ea31aa19569751ab
SstefanyPerez <TU_CORREO_DE_GITHUB>
2026-04-16 11:35:56 -0500

    correcion de conexion del bakend con el frontend

--- Frontend/src/app/App.tsx
+++ Frontend/src/app/App.tsx
@@ -1,24 +1,7 @@
 import AppRouter from './router/AppRouter'
 
-interface Estudiante {
-  id_estudiante: number;
-  numero_identificacion: string;
-  tipo_identificacion: string;
-  nombres: string;
-  apellidos: string;
-  correo_personal: string;
-  correo_institucional: string;
-  programa: string;
-  estado: string;
-  contador_inasistencias: number;
-  limite_inasistencias: number;
-  fecha_registro: string;
-}
-
 function App() {
   return <AppRouter />
 }
 
 export default App
-
-{/*renderizar el router*/}
