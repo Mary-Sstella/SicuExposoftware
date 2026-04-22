4af5b1e2780fd2ade18e36c517fd3f97a56644c8
SstefanyPerez <TU_CORREO_DE_GITHUB>
2026-04-22 10:41:08 -0500

    Rutas protegidas

--- Backend/src/modules/auth/auth.routes.js
+++ Backend/src/modules/auth/auth.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const controller = require('./auth.controller')
+const { loginValidator } = require('./auth.validator')
+const { validate } = require('../../shared/middleware/validate.middleware')
 
 const router = Router()
 
-router.post('/login', controller.login)
+router.post('/login', loginValidator, validate, controller.login)
 
 module.exports = router
