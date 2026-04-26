59ec587599a564317541a11d7e65805de7bed591
SstefanyPerez <TU_CORREO_DE_GITHUB>
2026-04-26 15:35:36 -0500

    agregar endpoints GET /estudiantes/dias y GET /estudiantes/:id/dias

--- Backend/src/modules/estudiantes/estudiante.routes.js
+++ Backend/src/modules/estudiantes/estudiante.routes.js
@@ -7,7 +7,9 @@ const { createEstudianteValidator, updateEstudianteValidator } = require('./estu
 const router = Router()
 
 router.get('/', verifyToken, controller.getEstudiantes)
+router.get('/dias', verifyToken, verifyRole('ADMIN'), controller.getEstudiantesDias)
 router.get('/:id', verifyToken, controller.getEstudianteById)
+router.get('/:id/dias', verifyToken, verifyRole('ADMIN'), controller.getEstudianteDias)
 router.post('/', verifyToken, verifyRole('ADMIN'), createEstudianteValidator, validate, controller.createEstudiante)
 router.put('/:id', verifyToken, verifyRole('ADMIN'), updateEstudianteValidator, validate, controller.updateEstudiante)
 router.delete('/:id', verifyToken, verifyRole('ADMIN'), controller.deleteEstudiante)
