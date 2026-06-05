f1fcffc60cdc6557fcc57d70b124e40b7c488273
SstefanyPerez <TU_CORREO_DE_GITHUB>
2026-06-04 19:55:11 -0500

    Correccion en Biometria Page

--- Frontend/src/features/biometria/pages/BiometriaPage.tsx
+++ Frontend/src/features/biometria/pages/BiometriaPage.tsx
@@ -47,7 +47,7 @@ function BiometriaPage() {
 
     useEffect(() => {
         const backendUrl = (import.meta.env.VITE_API_URL ?? '').replace('/sicu', '')
-        const wsUrl = backendUrl.replace('http', 'ws') + '/sicu/ws'
+        const wsUrl = backendUrl.replace('http', 'ws').replace('/sicu', '') + '/sicu/ws'
         const ws = new WebSocket(wsUrl)
         wsRef.current = ws
 
