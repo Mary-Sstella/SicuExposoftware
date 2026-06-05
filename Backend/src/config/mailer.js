ed9bf2f441d08e1d2d73367005a906991df9df51
SstefanyPerez <TU_CORREO_DE_GITHUB>
2026-06-04 20:24:47 -0500

    fix: Correcion de correos

--- Backend/src/config/mailer.js
+++ Backend/src/config/mailer.js
@@ -4,6 +4,7 @@ console.log('Enviando desde:', process.env.GMAIL_USER)
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    family: 4,
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
